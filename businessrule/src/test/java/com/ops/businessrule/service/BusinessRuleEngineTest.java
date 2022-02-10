948735b60375b4cbe2c54f92b63471d61ecefad5
K N Vishnu <vishnu.knehru@gmail.com>
2022-02-10 16:33:12 +0530

    business rule empty test cases

---
+++ businessrule/src/test/java/com/ops/businessrule/service/BusinessRuleEngineTest.java
@@ -0,0 +1,37 @@
+package com.ops.businessrule.service;
+
+import org.junit.jupiter.api.Test;
+
+public class BusinessRuleEngineTest {
+	
+	@Test
+	public void generatePackagingSlipAndAgentCommissionTest() {
+		
+	}
+	
+	@Test
+	public void generatePackagingSlipAndAgentCommissionForBookTest() {
+		
+	}
+
+	@Test
+	public void unknownProductTest() {
+		
+	}
+
+	@Test
+	public void membershipActivationTest() {
+		
+	}
+
+	@Test
+	public void membershipUpgradeTest() {
+		
+	}
+
+	@Test
+	public void addGiftTest() {
+		
+	}
+
+}
