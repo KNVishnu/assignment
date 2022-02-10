f8cd54d5d54da58249c87176e1894810c9532d81
K N Vishnu <vishnu.knehru@gmail.com>
2022-02-10 16:40:02 +0530

    Service to handle the order post successful payment

---
+++ businessrule/src/main/java/com/ops/businessrule/service/OrderProcessingService.java
@@ -0,0 +1,27 @@
+package com.ops.businessrule.service;
+
+import com.ops.businessrule.model.LineItem;
+import com.ops.businessrule.model.Order;
+import com.ops.businessrule.model.ProductType;
+
+/**
+ * Service to handle the order post successful payment
+ * @author Vishnu K N
+ */
+public interface OrderProcessingService {
+
+	/**
+	 * Process specific instructions based on the product type
+	 * @param Order order
+	 * @param LineItem lineItem
+	 * @return String
+	 */
+	String process(Order order, LineItem lineItem);
+	
+	/**
+	 * Check the service supported for the product type
+	 * @param ProductType productType
+	 * @return boolean
+	 */
+	boolean isSupportedServive(ProductType productType);
+}
