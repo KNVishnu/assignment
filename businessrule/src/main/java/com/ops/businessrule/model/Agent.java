b3dcdfc4a60acd595a29736cff1395d0b203d321
K N Vishnu <vishnu.knehru@gmail.com>
2022-02-10 16:35:57 +0530

    model to encapsulate agent details

---
+++ businessrule/src/main/java/com/ops/businessrule/model/Agent.java
@@ -0,0 +1,46 @@
+package com.ops.businessrule.model;
+
+/**
+ * Agent model
+ * @author Vishnu K N
+ *
+ */
+public class Agent {
+
+	private Long agentId;
+	private String name;
+	private Long addressId;
+	private String email;
+	private String phoneNo;
+	
+	public Long getAgentId() {
+		return agentId;
+	}
+	public void setAgentId(Long agentId) {
+		this.agentId = agentId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(Long addressId) {
+		this.addressId = addressId;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+}
